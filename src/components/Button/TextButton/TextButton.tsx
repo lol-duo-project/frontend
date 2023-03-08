5a95fad5b664b87fc65a9faaaec9f119fe36a243
MoveRoad <oio3215@naver.com>
2023-03-08 20:24:05 +0900

    [Feat] :: Button 용도별로 분기

---
+++ src/components/Button/TextButton/TextButton.tsx
@@ -0,0 +1,17 @@
+import Style, { generateClassNames } from "@src/utils/Style"
+
+export const TextButton = () => {
+  return (
+    <Style css={css}>
+      <button className={`${CLASSNAMES.}`}></button>
+    </Style>
+  )
+}
+
+const CLASSNAMES = generateClassNames({
+
+})
+
+const css = `
+  
+`
