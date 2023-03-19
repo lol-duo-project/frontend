6a8694ee4c4d1cd9cdf6ad75fd7dfdf6aae07e5b
MoveRoad <oio3215@naver.com>
2023-03-19 21:42:31 +0900

    [Feat] :: 공용 style 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,18 @@
+export const gray000 = '#F9F9F9';
+export const gray050 = '#E5E5E5';
+export const gray100 = '#B5B5B5';
+export const gray200 = '#676767';
+export const gray300 = '#555555';
+export const gray400 = '#3F3F3F';
+
+export const background = '#212121';
+export const card = '#2F2F2F';
+export const hover = '#424242';
+
+export const primaryBlue = '#466CD2';
+export const secondaryBeige = '#C8AA6E';
+
+export const TitleGuide = '#38289C / 75';
+export const yetGuide = '#9C2883 / 75';
+export const VisibleGuide = '#444247 / 40';
+export const BgGuide = '#F9F9FC';
